9f767207d1019cc9b958e879c62b6978ea7ccdea
God of War <jamel.eggleston@gmail.com>
2021-08-25 22:24:02 -0400

    added some pseudo to my js file.

--- Develop/script.js
+++ Develop/script.js
@@ -2,4 +2,27 @@
 
 $("#currentday").text(moment().format("dddd, mmmm Do, YYYY"));
 
-const container = $(".container")
+const container = $(".container");
+
+// This will add all the rows to the schedule container element
+
+const addHours = () => {
+    for (let i =0; i < 9; i++){
+        const nextRow = $("<div>");
+        nextRow.attr("class", "row");
+        container.append(nextRow);
+
+        const nextHour = $("<div>")
+    }
+
+ // Set text for each textarea block from local storage if it's there, and leave it blank if not
+
+ // need to Add save a button icon to each save button
+
+} 
+
+// Check the current time and apply styling to textarea elements based on whether they are past, present, or future
+
+// Create all elements on page load
+
+// need to add a event listener to each save button in order to set local storage
