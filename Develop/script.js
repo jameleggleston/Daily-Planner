be9001d741807ab4c4dc136264ff72099bd3ab31
God of War <jamel.eggleston@gmail.com>
2021-08-25 23:57:24 -0400

    finished script file

--- Develop/script.js
+++ Develop/script.js
@@ -6,23 +6,66 @@ const container = $(".container");
 
 // This will add all the rows to the schedule container element
 
-const addHours = () => {
+const addHours =() => {
     for (let i =0; i < 9; i++){
         const nextRow = $("<div>");
         nextRow.attr("class", "row");
         container.append(nextRow);
 
-        const nextHour = $("<div>")
-    }
+        const nextHour = $("<div>");
+        nextHour.attr("class", "hour");
+        // this should set text content for our hour blocks.
+        nextHour.text(moment().hour(i + 9).format("hA"));    
    
-    // need to Set text for each text area block from local storage if it's there, and leave it blank if not
-   
-    // need to Add save a button icon to each save button
+        // need to Set text for each text area block from local storage if it's there, and leave it blank if not
+        let nextText = $("<textarea>");
+        nextText.attr("class", "time-block");
+        nextText.text(localStorage.getItem(`textArea #${i+1}`) || "");
 
+        let nextSave = $("<button>");
+        nextSave.attr("class", "saveBtn");
+
+        nextRow.append(nextHour);
+        nextRow.append(nextText);
+        nextRow.append(nextSave);
+
+        // need to Add save a button icon to each save button
+        let nextIconContainer = $("<i>");
+        nextSave.append(nextIconContainer);
+        let nextIcon = $("<img>");
+        nextIcon.attr({
+        src: "/Assets/Save-icon2.png",
+        width: "15px",
+        height: "15px",
+        alt: "Floppy Disk icon"
+        });
+        nextIconContainer.append(nextIcon);
+
+    }
 } 
 
 // Check the current time and apply styling to textarea elements based on whether they are past, present, or future
+const checkTime =()=> {
+    let rightNow = moment().hour();
+    $("textarea").each(function(i) {
+        let thisBlockHourValue = parseInt(moment().hour(i + 9).format("H"));
+        if (thisBlockHourValue < rightNow) {
+            $(this).addClass("past");
+        } else if (thisBlockHourValue === rightNow) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("future");
+        }
+    });
+}
 
 // Create all elements on page load
-
+addHours();
+checkTime();
 // need to add a event listener to each save button in order to set local storage
+$(".saveBtn").each(function(i) {
+    $(this).on("click", function() {
+        let thisHourText = $(this).prev();
+        localStorage.setItem(`textArea #${i+1}`, thisHourText.val());
+    });
+});
